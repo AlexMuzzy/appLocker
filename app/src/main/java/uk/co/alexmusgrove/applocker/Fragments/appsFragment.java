d7966d1721de1cb6d4b6cd49cf34b4b3bb6987d6
AlexMuzzy <alexmuzzy33@gmail.com>
2020-04-11 20:30:05 +0100

    Locked apps fragment & foreground service progress.

--- app/src/main/java/uk/co/alexmusgrove/applocker/Fragments/appsFragment.java
+++ app/src/main/java/uk/co/alexmusgrove/applocker/Fragments/appsFragment.java
@@ -1,10 +1,8 @@
 package uk.co.alexmusgrove.applocker.Fragments;
 
 import android.content.ComponentName;
-import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
@@ -20,37 +18,40 @@ import android.widget.CompoundButton;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
+import uk.co.alexmusgrove.applocker.Activities.HomeActivity;
 import uk.co.alexmusgrove.applocker.Adapters.appAdapter;
-import uk.co.alexmusgrove.applocker.Database.AppContentProvider;
-import uk.co.alexmusgrove.applocker.Database.AppSQLiteDBHelper;
 import uk.co.alexmusgrove.applocker.Helpers.appItem;
+import uk.co.alexmusgrove.applocker.Helpers.appItems;
 import uk.co.alexmusgrove.applocker.R;
 
 public class appsFragment extends Fragment {
 
+    private ArrayList<appItem> appItemArrayList;
+    private appItems appItems;
+    private boolean lockFragmentState;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.activity_apps, container, false);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        appItemArrayList = ((HomeActivity) Objects.requireNonNull(getActivity())).getAppItems();
+        appItems = new appItems(getActivity());
         buildRecyclerView();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        generateAppItems();
     }
-
-    private ArrayList<appItem> appItems = new ArrayList<>();
 
     public void buildRecyclerView () {
         RecyclerView recyclerView = getView().findViewById(R.id.user_recycler_view);
@@ -62,66 +63,28 @@ public class appsFragment extends Fragment {
         recyclerView.setLayoutManager(recyclerViewLayoutManager);
 
         // specify an adapter
-        appAdapter adapter = new appAdapter(appItems);
+        appAdapter adapter = new appAdapter(appItemArrayList);
         recyclerView.setAdapter(adapter);
 
         adapter.setOnItemClickListener(
-                (position) -> launchAppIntent(appItems.get(position).getmPackageName())
+                (position) -> appItems.launchAppIntent(appItemArrayList.get(position).getmPackageName())
         );
         adapter.setOnCheckedChangeListener((CompoundButton buttonView, int position, boolean isChecked) -> {
             if (buttonView.isShown()) {
                 if (isChecked) {
-                    addApp(appItems.get(position), position);
+                    appItems.addApp(appItemArrayList.get(position), position);
                 }
                 if(!isChecked){
-                    removeApp(appItems.get(position), position);
+                    appItems.removeApp(appItemArrayList.get(position), position);
                 }
                 Toast.makeText(
                         getActivity(),
                         (isChecked)
-                                ? "Locked " + appItems.get(position).getmAppName()
-                                : "Unlocked " + appItems.get(position).getmAppName(),
+                                ? "Locked " + appItemArrayList.get(position).getmAppName()
+                                : "Unlocked " + appItemArrayList.get(position).getmAppName(),
                         Toast.LENGTH_SHORT).show();
             }
         });
-    }
-
-    public void launchAppIntent(String packageName) {
-        Intent intent = new Intent();
-        intent.setPackage(packageName);
-
-        PackageManager pm = getActivity().getPackageManager();
-        List<ResolveInfo> resolveInfos = pm.queryIntentActivities(intent, 0);
-        resolveInfos.sort(new ResolveInfo.DisplayNameComparator(pm));
-
-        if(resolveInfos.size() > 0) {
-            ResolveInfo launchable = resolveInfos.get(0);
-            ActivityInfo activity = launchable.activityInfo;
-            ComponentName name=new ComponentName(activity.applicationInfo.packageName,
-                    activity.name);
-            Intent i=new Intent(Intent.ACTION_MAIN);
-
-            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-            i.setComponent(name);
-
-            startActivity(i);
-        }
-    }
-
-    public void addApp (appItem appItem, int position) {
-        ContentValues values = new ContentValues();
-        values.put(AppSQLiteDBHelper.COLUMN_PACKAGENAME, appItem.getmPackageName());
-        getActivity().getContentResolver().insert(AppContentProvider.APP_CONTENT_URI, values);
-        appItems.get(position).setmLocked(true);
-    }
-
-    public void removeApp (appItem appItem, int position) {
-        getActivity().getContentResolver().delete(
-                AppContentProvider.APP_CONTENT_URI,
-                AppSQLiteDBHelper.COLUMN_PACKAGENAME + " = '" + appItem.getmPackageName() + "'",
-                null
-        );
-        appItems.get(position).setmLocked(false);
     }
 
     @Override
--- app/src/main/java/uk/co/alexmusgrove/applocker/Helpers/appItems.java
+++ app/src/main/java/uk/co/alexmusgrove/applocker/Helpers/appItems.java
@@ -1,26 +1,35 @@
 package uk.co.alexmusgrove.applocker.Helpers;
 
+import android.content.ComponentName;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import uk.co.alexmusgrove.applocker.Database.AppContentProvider;
 import uk.co.alexmusgrove.applocker.Database.AppSQLiteDBHelper;
 
 public class appItems {
     public ArrayList<appItem> appItems;
-    static Context context;
+    private Context context;
 
     public appItems(Context context) {
         this.context = context;
-        if (appItems == null){
+        //static invocation of app items
+        if (appItems == null) {
             appItems = new ArrayList<>();
             generateAppItems();
         }
     }
+
 
     private void generateAppItems() {
         final PackageManager pm = context.getPackageManager();
@@ -33,7 +42,7 @@ public class appItems {
                 String appName = (String) pm.getApplicationLabel(packageInfo);
                 ArrayList<String> appList = AppSQLiteDBHelper.getAllApps(context);
                 //append details to appItem class
-                if (!packageInfo.packageName.equals("uk.co.alexmusgrove.applocker")){
+                if (!packageInfo.packageName.equals("uk.co.alexmusgrove.applocker")) {
                     appItems.add(new appItem(
                             appName, //name of application
                             packageInfo.packageName, // unique package name of application
@@ -44,5 +53,44 @@ public class appItems {
             }
         }
         Collections.sort(appItems, (app1, app2) -> app1.getmAppName().compareToIgnoreCase(app2.getmAppName()));
+    }
+
+
+    public void addApp(appItem appItem, int position) {
+        ContentValues values = new ContentValues();
+        values.put(AppSQLiteDBHelper.COLUMN_PACKAGENAME, appItem.getmPackageName());
+        context.getContentResolver().insert(AppContentProvider.APP_CONTENT_URI, values);
+        appItems.get(position).setmLocked(true);
+    }
+
+    public void removeApp(appItem appItem, int position) {
+        context.getContentResolver().delete(
+                AppContentProvider.APP_CONTENT_URI,
+                AppSQLiteDBHelper.COLUMN_PACKAGENAME + " = '" + appItem.getmPackageName() + "'",
+                null
+        );
+        appItems.get(position).setmLocked(false);
+    }
+
+    public void launchAppIntent(String packageName) {
+        Intent intent = new Intent();
+        intent.setPackage(packageName);
+
+        PackageManager pm = context.getPackageManager();
+        List<ResolveInfo> resolveInfo = pm.queryIntentActivities(intent, 0);
+        resolveInfo.sort(new ResolveInfo.DisplayNameComparator(pm));
+
+        if (resolveInfo.size() > 0) {
+            ResolveInfo launchable = resolveInfo.get(0);
+            ActivityInfo activity = launchable.activityInfo;
+            ComponentName name = new ComponentName(activity.applicationInfo.packageName,
+                    activity.name);
+            Intent i = new Intent(Intent.ACTION_MAIN);
+
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+            i.setComponent(name);
+
+            context.startActivity(i);
+        }
     }
 }
